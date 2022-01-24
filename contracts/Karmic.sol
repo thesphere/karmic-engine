f33a613986089c6e080587f6405eb67ff995fdc3
Konstantin <BritikovKI@Yandex.ru>
2022-01-25 00:28:07 +0300

    payable done

--- contracts/Karmic.sol
+++ contracts/Karmic.sol
@@ -6,18 +6,49 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./Badger.sol";
 
 contract Karmic is Badger{
-    mapping(address => uint256) private boxTokenTiers;
+    mapping(address => BoxToken) private boxTokenTiers;
     uint256 boxTokenCounter;
+    uint256 threshold;
 
-    constructor(string memory _newBaseUri) Badger(_newBaseUri) {}
+    struct BoxToken{
+        uint256 id;
+        uint256 amount;
+    }
+
+    modifier isBoxToken(address token){
+        require(boxTokenTiers[token].id != 0, "It is not a box token");
+        _;
+    }
+
+
+
+    constructor(string memory _newBaseUri) Badger(_newBaseUri) {
+        boxTokenCounter = 1;
+    }
+
+    fallback() external payable {
+        boxTokenTiers[msg.sender].amount = msg.value;
+    }
+
+    function bondToMint(address token, uint256 amount) public isBoxToken(token) {
+
+        require(ERC20(token).transferFrom(msg.sender, address(this), amount),
+            "Failed to withdraw stakeholder's ERC20 tokens");
+        bytes memory data;
+        if(boxTokenTiers[token].amount > threshold){
+            _mint(msg.sender, boxTokenTiers[token].id, amount, data);
+        } else {
+            _mint(msg.sender, 0, amount, data);
+        }
+    }
 
     function addBoxTokens(address[] memory tokens, string[] calldata tierUris) external onlyOwner {
         uint256 counter = boxTokenCounter;
 
         for(uint8 i; i< tokens.length; i++) {
             address token = tokens[i];
-            require(boxTokenTiers[token] == 0, "DUPLICATE_TOKEN");
-            boxTokenTiers[token] = counter + 1;
+            require(boxTokenTiers[token].id == 0, "DUPLICATE_TOKEN");
+            boxTokenTiers[token].id = counter + 1;
             createTokenTier(counter + 1, tierUris[i], false, token);
             counter++;
         }
@@ -39,7 +70,7 @@ contract Karmic is Badger{
         for(uint8 i; i < boxTokens.length; i++) {
             token = boxTokens[i];
             uint256 amount = IERC20(token).balanceOf(msg.sender);
-            uint256 tokenId = boxTokenTiers[token];
+            uint256 tokenId = boxTokenTiers[token].id;
             IERC20(token).transferFrom(msg.sender, address(this), amount);
             _mint(msg.sender, tokenId, amount, data);
         }
